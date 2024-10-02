ecaac0eeb92ad9050ec47473de72f919976066db
施均達 <hpes0101@gmail.com>
2024-10-02 15:37:14 +0800

    商品功能 - 商品列表(分頁)

--- springboot-mall/src/main/java/com/junda/springbootmall/dao/impl/ProductDaompl.java
+++ springboot-mall/src/main/java/com/junda/springbootmall/dao/impl/ProductDaompl.java
@@ -34,6 +34,7 @@ public class ProductDaompl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢條件
         if (productQueryParams.getCategory() != null){
             sql = sql + " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -43,7 +44,14 @@ public class ProductDaompl implements ProductDao {
             sql = sql + " AND product_name Like :search";
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
+
+        // 排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        // 分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;
