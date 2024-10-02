6e7200f53372781413085847ad66262dfb060da5
施均達 <hpes0101@gmail.com>
2024-10-02 16:03:44 +0800

    商品功能 - 商品列表(分頁 + 總比數)

--- springboot-mall/src/main/java/com/junda/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/junda/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.junda.springbootmall.dto.ProductQueryParams;
 import com.junda.springbootmall.dto.ProductRequest;
 import com.junda.springbootmall.model.Product;
 import com.junda.springbootmall.service.ProductService;
+import com.junda.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -26,7 +28,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // 查詢條件
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String  search,
@@ -47,9 +49,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // 取得 product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // 取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
