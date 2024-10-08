a5e775f178e7092ba0847fc29173be4c0fc7e2d9
施均達 <hpes0101@gmail.com>
2024-10-08 16:11:57 +0800

    帳號功能 - 檢查註冊 email , 隱藏密碼

--- springboot-mall/src/main/java/com/junda/springbootmall/model/User.java
+++ springboot-mall/src/main/java/com/junda/springbootmall/model/User.java
@@ -1,11 +1,20 @@
 package com.junda.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 
 public class User {
     private Integer userId;
+
+    @JsonProperty("e_mail")
     private String email;
+
+    // 隱藏變數
+    @JsonIgnore
     private String password;
+
     private Date createdDate;
     private Date lastModifiedDate;
 
