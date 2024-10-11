3434591b983e2b7af24230a724bb61c0f13c5d32
施均達 <hpes0101@gmail.com>
2024-10-11 22:59:20 +0800

    訂單功能 - 查詢訂單列表

--- springboot-mall/src/main/java/com/junda/springbootmall/dao/OrderDao.java
+++ springboot-mall/src/main/java/com/junda/springbootmall/dao/OrderDao.java
@@ -1,11 +1,16 @@
 package com.junda.springbootmall.dao;
 
+import com.junda.springbootmall.dto.OrderQueryParams;
 import com.junda.springbootmall.model.Order;
 import com.junda.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
@@ -14,4 +19,6 @@ public interface OrderDao {
     Integer createOrder(Integer userId,Integer totalAmount);
 
     void createOrderItems(Integer ordertId, List<OrderItem> orderItemList);
+
+
 }
--- springboot-mall/src/main/java/com/junda/springbootmall/service/OrderService.java
+++ springboot-mall/src/main/java/com/junda/springbootmall/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.junda.springbootmall.service;
 
 import com.junda.springbootmall.dto.CreateOrderRequest;
+import com.junda.springbootmall.dto.OrderQueryParams;
 import com.junda.springbootmall.model.Order;
+import com.junda.springbootmall.model.OrderItem;
+
+import java.util.List;
 
 public interface OrderService {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
